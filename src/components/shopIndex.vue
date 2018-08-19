c853a42250e8fddbcd4a7aa2700a964d7251462c
Masato TSUTSUMI <masato.11.soccer@gmail.com>
2018-08-19 14:24:59 +0900

    init

---
+++ src/components/shopIndex.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="shop-item">
+    <router-link :to="{ name: 'shopDetail', params: { id: id }}">
+        <img :src="shop.image" width="300px">
+        <h1 class="is-size-4">{{shop.name}}</h1>
+    </router-link>
+  </div>
+</template>
+
+<script lang="js">
+import Vue from 'vue'
+import shopDetail from './shopDetail.vue'
+import { Repository } from '../../utils/repository.js';
+
+export default Vue.extend({
+  name: 'shopIndex',
+  components:{
+    shopDetail
+  },
+  props: ['shop', 'id']
+})
+</script>
+
+<style scoped>
+.shop-item {
+  margin: 40px;
+}
+</style>
---
+++ src/components/suggestion.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="suggestion">
+    <div class="suggest-item" v-for="(good, key) in goods" :key="key">
+        <router-link :to="{ name: 'goodsDetail', params: { id: key }}">
+          <img :src="good.image" width="300px" v-if="good">
+        </router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="js">
+import Vue from 'vue';
+import { Repository } from '../../utils/repository.js';
+
+export default Vue.extend({
+  name:'suggestion',
+  data() {
+      return {
+        goods: []
+    }
+  },
+  mounted() {
+    const repository = new Repository()
+    this.goods = repository.getGoods()
+  }
+  })
+</script>
+
+<style scoped>
+.suggestion {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.suggest-item {
+  margin: 50px;
+}
+</style>
